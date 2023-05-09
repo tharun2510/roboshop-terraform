b6b1bd99303745a5a4c4c7346e0963976a6761e4
tarun <guntuputharunkumar95@gmail.com>
2023-05-10 04:29:18 +0530

    we have added no

--- roboshop.tf
+++ roboshop.tf
@@ -12,43 +12,43 @@ data "aws_security_group" "sg" {
 variable "components" {
   default = {
     frontend = {
-      name          = frontend
+      name          = "frontend"
       instance_type = "t3.micro"
     }
     mongodb = {
-      name          = mongodb
+      name          = "mongodb"
       instance_type = "t3.micro"
     }
     catalogue = {
-      name          = catalogue
+      name          = "catalogue"
       instance_type = "t3.micro"
     }
     redis = {
-      name          = redis
+      name          = "redis"
       instance_type = "t3.micro"
     }
     user = {
-      name          = user
+      name          = "user"
       instance_type = "t3.micro"
     }
     mysql = {
-      name          = mysql
+      name          = "mysql"
       instance_type = "t3.micro"
     }
     cart = {
-      name          = cart
+      name          = "cart"
       instance_type = "t3.micro"
     }
     shipping = {
-      name          = shipping
+      name          = "shipping"
       instance_type = "t3.micro"
     }
     rabbitmq = {
-      name          = rabbitmq
+      name          = "rabbitmq"
       instance_type = "t3.micro"
     }
     payment = {
-      name          = payment
+      name          = "payment"
       instance_type = "t3.micro"
     }
   }
