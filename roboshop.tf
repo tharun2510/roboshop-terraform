971dcdac2fd621919288f66bbd4cb1a9bd6c01cd
tarun <guntuputharunkumar95@gmail.com>
2023-05-10 04:26:33 +0530

    we have added no

--- roboshop.tf
+++ roboshop.tf
@@ -10,18 +10,59 @@ data "aws_security_group" "sg" {
 
 
 variable "components" {
-  default=["frontend","mongodb","mysql"]
+  default = {
+    frontend = {
+      name          = frontend
+      instance_type = "t3.micro"
+    }
+    mongodb = {
+      name          = mongodb
+      instance_type = "t3.micro"
+    }
+    catalogue = {
+      name          = catalogue
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name          = redis
+      instance_type = "t3.micro"
+    }
+    user = {
+      name          = user
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = mysql
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = cart
+      instance_type = "t3.micro"
+    }
+    shipping = {
+      name          = shipping
+      instance_type = "t3.micro"
+    }
+    rabbitmq = {
+      name          = rabbitmq
+      instance_type = "t3.micro"
+    }
+    payment = {
+      name          = payment
+      instance_type = "t3.micro"
+    }
+  }
 }
 
 
 
 resource "aws_instance" "instances" {
-  count= length(var.components)
+  for_each = var.components
   ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids  = [ data.aws_security_group.sg.id]
+  instance_type =each.value["instance_type"]
+  vpc_security_group_ids  = [ data.aws_security_group.sg.id ]
 tags = {
-    Name = var.components[count.index]
+    Name = each.value["name"]
   }
 }
 
