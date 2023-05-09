e1820e3ab01c9cbcdc8e33dd04a5568e7c095a55
tarun <guntuputharunkumar95@gmail.com>
2023-05-10 00:45:44 +0530

    we have added no

---
+++ roboshop.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.devops_practice_ami.image_id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "frontend"
+
+  }
+}
+
+output "frontend_pulic_ip" {
+  value = aws_instance.frontend
+}
+
+
+data "aws_ami" "devops_practice_ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = [973714476881]
+}
