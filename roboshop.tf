a64a0350d6fe95eee5b2fb30eedb7f7bea60106b
tarun <guntuputharunkumar95@gmail.com>
2023-05-10 01:15:48 +0530

    we have added no

--- roboshop.tf
+++ roboshop.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.devops_practice_ami.image_id
   instance_type = "t3.micro"
-
-  tags = {
+  vpc_security_group_ids  = [ data.aws_security_group.test.id]
+tags = {
     Name = "frontend"
 
   }
 }
 
+
 output "frontend_pulic_ip" {
- value = aws_instance.frontend.instance_type
+ value = aws_instance.frontend
 }
 
 
@@ -17,4 +18,13 @@ data "aws_ami" "devops_practice_ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = [973714476881]
+}
+
+
+data "aws_security_group" "test" {
+name   = "allow-all"
+}
+
+output "aws_security" {
+  value = data.aws_security_group
 }
