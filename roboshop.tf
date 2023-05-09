dc640a02720621fd26fc1c43b40ed5a6865ba25e
tarun <guntuputharunkumar95@gmail.com>
2023-05-10 02:53:21 +0530

    we have added no

--- roboshop.tf
+++ roboshop.tf
@@ -1,30 +1,31 @@
-resource "aws_instance" "frontend" {
-  ami           = data.aws_ami.devops_practice_ami.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids  = [ data.aws_security_group.test.id]
-tags = {
-    Name = "frontend"
-
-  }
-}
-
-
-output "frontend_pulic_ip" {
- value = aws_instance.frontend
-}
-
-
-data "aws_ami" "devops_practice_ami" {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = [973714476881]
 }
 
-
-data "aws_security_group" "test" {
-name   = "allow-all"
+data "aws_security_group" "sg" {
+  name   = "allow-all"
 }
 
-output "aws_security" {
-  value = data.aws_security_group.test
+
+variable "components" {
+  default=[frontend,mongodb,mysql]
 }
+
+
+
+resource "aws_instance" "instances" {
+  count= length(var.components)
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids  = [ data.aws_security_group.sg.id]
+tags = {
+    Name = var.components[count.index]
+  }
+}
+
+
+
+
+
