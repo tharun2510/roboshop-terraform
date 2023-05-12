20514c24063a1909e9d94de114f173937df9af0d
tarun <guntuputharunkumar95@gmail.com>
2023-05-12 06:00:15 +0530

    we have added no

--- servers.tf
+++ servers.tf
@@ -39,7 +39,7 @@ resource "null_resource" "null" {
 
     inline = [
       "rm -rf roboshop-shell",
-      "git clone https://github.com/tharun2510/roboshop-shell1.git",
+      "git clone https://github.com/tharun2510/roboshop-shell.git",
       "cd roboshop-shell",
       "sudo bash  ${each.key}.sh"
     ]
