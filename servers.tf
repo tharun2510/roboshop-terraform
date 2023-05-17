cf9317f02a47ce64902af54e7a2468071a500fb4
tarun <guntuputharunkumar95@gmail.com>
2023-05-17 08:45:33 +0530

    we have added no

--- servers.tf
+++ servers.tf
@@ -3,6 +3,6 @@ module "roboshop"  {
   source = "./module"
   components_name=each.value["name"]
   instance_type_name=each.value["instance_type"]
-  password=lookup(each.value,"password",null)
+  password=lookup(each.value,"password","null")
 
 }
